95da728b86d8f4d4d6549fb96be68bc804119ddb
Michael Duncan <m.duncan1488@gmail.com>
2023-11-09 09:33:39 +1300

    Update PINS.h

--- RGB_Torch/PINS.h
+++ RGB_Torch/PINS.h
@@ -11,12 +11,14 @@
 #define GREEN 0  // PB0 physical pin 5
 #define BLUE 4   // PB4 physical pin 3
 #define TOUCH 2  // PB2 physical pin 7
-#elif defined(_ESP8266_)
 
-#define RED 1    // PB1 physical pin 6
-#define GREEN 0  // PB0 physical pin 5
-#define BLUE 4   // PB4 physical pin 3
-#define TOUCH 2  // PB2 physical pin 7
+#elif defined(ESP8266)
+
+#define RED 13    // PB1 physical pin 6
+#define GREEN 14  // PB0 physical pin 5
+#define BLUE 15   // PB4 physical pin 3
+#define TOUCH 2       // PB2 physical pin 7
+
 #else
 #error "Unsupported board selected"
 
